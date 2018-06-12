7830b3440e9ba47582d147a1292de375d310762d
Weuler Borges <weulerfilho@outlook.com>
2018-06-12 19:33:24 -0300

    Merge sensação

--- paginaagendamento.php
+++ paginaagendamento.php
@@ -61,6 +61,57 @@ require_once "conexaobd.php";
 
 
   </script>
+
+
+ <?php
+    if ($_SERVER["REQUEST_METHOD"]=="POST" && isset($_POST["especialidademedica"]) &&  $_POST["nomemedico"] && $_POST["dataconsulta"] && $_POST["horariodisponivel"] && $_POST["paciente"] && $_POST["telefonepaciente"]) {
+
+
+      $conn = conectabd();
+    
+      $especialidade = $_POST["especialidademedica"];
+      $medico = $_POST["nomemedico"];
+      $data = $_POST["dataconsulta"];
+      $horario = $_POST["horariodisponivel"];
+      $paciente = $_POST["paciente"];
+      $telefone = $_POST["telefonepaciente"];
+    
+      //adiciona novo paciente
+      $paciente_query = "INSERT INTO Paciente(paciente, telefone) VALUES ('$paciente', '$telefone')";
+    
+      $res = $conn->query($paciente_query);
+    
+      if(! $res) {
+        throw new Exception('Ocorreu uma falha ao criar novo paciente: ' . $conn->error);
+      }
+    
+      $id_paciente = $conn->insert_id;
+    
+      //pega o id do medico selecionado
+      $medico_query = "SELECT id FROM funcionario WHERE nome='$medico'";
+    
+      $res = $conn->query($medico_query); 
+    
+      if(! $res) {
+        throw new Exception('Ocorreu uma falha buscar id do medico: ' . $conn->error);
+      }
+    
+      $row = $res->fetch_assoc();
+      $id_funcionario = $row["id"];
+    
+      //realiza agendamento
+      $agenda_query = "INSERT INTO Agenda(codAgendamento, dataconsulta, horaconsulta, codFuncionario, codPaciente) 
+                        VALUES (null, '$data', '$horario', '$id_funcionario', '$id_paciente')";
+    
+      $res = $conn->query($agenda_query);
+    
+      if(! $res) {
+        throw new Exception('Ocorreu uma falha ao inserir na Agenda: ' . $conn->error);
+      } else {
+        echo "<span style='padding-left: 15px' class='success-message'> Agendamento realizado com sucesso!! </span>";
+      }
+    }
+  ?>
 <!-- <script>
 
 jQuery(function ($) {
@@ -86,82 +137,29 @@ jQuery(function ($) {
   <div class="container agendamento card ">
     <form class="form-horizontal" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
       <div class="form-group">
+          <label class="control-label col-sm-3" for="especialidademedica">Especialidade Médica:</label>
+            <div class="col-sm-3">
+              <select id="especialidademedica" name="especialidademedica" class="form-control" onchange="buscaMedico(this.value)">
+                <option value="" selected>Selecione</option>
+                <option value="CirurgiaoDentista">Cirurgião Dentista</option>
+                <option value="Odontopediatra">Odontopediatra</option> <!-- saúde bucal das crianças -->
+                <option value="Odontohebiatria">Odontohebiatria</option> <!-- saúde bucal dos adolescentes -->
+                <option value="Ortodontista">Ortodontista</option> <!-- aparelhos ortodônticos -->
+                <option value="OdontologiaEstetica">Odontologia Estética</option><!-- clareamentos dentais, uso de resinas e peeling gengival -->
+                <option value="Endodontista">Endodontista</option><!-- tratamento de canal  -->
+                <option value="Periodontista">Periodontista</option><!-- cuidados relacionados a doenças de gengiva -->
+                <option value="Protesista">Protesista</option> <!-- reabilitação bucal: estética, fonética e mastigação -->
+                <option value="Implantodontista">Implantodontista</option> <!-- inserção de protéses fixas/implantes -->
+              </select>
+            </div>
 
-        <?php
-          if ($_SERVER["REQUEST_METHOD"]=="POST" && isset($_POST["especialidademedica"]) &&  $_POST["nomemedico"] && $_POST["dataconsulta"] && $_POST["horariodisponivel"] && $_POST["nomepaciente"] && $_POST["telefonepaciente"]) {
-
-
-            $conn = conectabd();
-          
-            $especialidade = $_POST["especialidademedica"];
-            $medico = $_POST["nomemedico"];
-            $data = $_POST["dataconsulta"];
-            $horario = $_POST["horariodisponivel"];
-            $paciente = $_POST["paciente"];
-            $telefone = $_POST["telefonepaciente"];
-          
-            //adiciona novo paciente
-            $paciente_query = "INSERT INTO Paciente(paciente, telefone) VALUES ('$paciente', '$telefone')";
-          
-            $res = $conn->query($paciente_query);
-          
-            if(! $res) {
-              throw new Exception('Ocorreu uma falha ao criar novo paciente: ' . $conn->error);
-            }
-          
-            $id_paciente = $conn->insert_id;
-          
-            //pega o id do medico selecionado
-            $medico_query = "SELECT id FROM funcionario WHERE nome='$medico'";
-          
-            $res = $conn->query($medico_query); 
-          
-            if(! $res) {
-              throw new Exception('Ocorreu uma falha buscar id do medico: ' . $conn->error);
-            }
-          
-            $row = $res->fetch_assoc();
-            $id_funcionario = $row["id"];
-          
-            //realiza agendamento
-            $agenda_query = "INSERT INTO Agenda(codAgendamento, dataconsulta, horaconsulta, codFuncionario, codPaciente) 
-                              VALUES (null, '$data', '$horario', '$id_funcionario', '$id_paciente')";
-          
-            $res = $conn->query($agenda_query);
-          
-            if(! $res) {
-              throw new Exception('Ocorreu uma falha ao inserir na Agenda: ' . $conn->error);
-            } else {
-              echo "<span style='padding-left: 15px' class='success-message'> Agendamento realizado com sucesso!! </span>";
-            }
-          }
-        ?>
-
-        <div class="form-group">
-          <label class="control-label col-sm-2" for="especialidademedica">Especialidade Médica:</label>
-          <div class="col-sm-2">
-            <select id="especialidademedica" name="especialidademedica" class="form-control" onchange="buscaMedico(this.value)">
-              <option value="" selected>Selecione</option>
-              <option value="cirurgiaodentista">Cirurgião Dentista</option>
-              <option value="odontopediatra">Odontopediatra</option> <!-- saúde bucal das crianças -->
-              <option value="odontohebiatria">Odontohebiatria</option> <!-- saúde bucal dos adolescentes -->
-              <option value="ortodontista">Ortodontista</option> <!-- aparelhos ortodônticos -->
-              <option value="odontologiaestetica">Odontologia Estética</option><!-- clareamentos dentais, uso de resinas e peeling gengival -->
-              <option value="endodontista">Endodontista</option><!-- tratamento de canal  -->
-              <option value="periodontista">Periodontista</option><!-- cuidados relacionados a doenças de gengiva -->
-              <option value="protesista">Protesista</option> <!-- reabilitação bucal: estética, fonética e mastigação -->
-              <option value="implantodontista">Implantodontista</option> <!-- inserção de protéses fixas/implantes -->
-            </select>
-          </div>
-
-          <label class="control-label col-sm-2" for="nomemedico">Médico:</label>
-          <div class="col-sm-2">
-            <select id="nome" name="nomemedico" class="form-control" onchange="buscaHorarios()" required>
-              <option value="" selected>Selecione</option>
-            </select>
-          </div>
-        </div>
-
+            <label class="control-label col-sm-2" for="medico">Médico:</label>
+            <div class="col-sm-2">
+              <select id="nome" name="nomemedico" class="form-control" onchange="buscaHorarios()" required>
+                <option value="" selected>Selecione</option>
+              </select>
+            </div>
+      </div>
 
         <div class="form-group">
             <label class="control-label col-sm-3" for="dataconsulta">Data da consulta:</label>
